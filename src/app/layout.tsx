0499ec0822c3761ef0b1bee2e4c55fc07d935cb8
humberto ragazzini <beto_vicio@hotmail.com>
2025-07-20 22:17:34 +0200

    added overlay menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import OverlayMenu from "@/compontents/atoms/OverlayMenu";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="max-w-[1550px] mx-auto">
-          <h1 className="text-2xl text-white">Layout navbar</h1>
+          <OverlayMenu></OverlayMenu>
         </div>
         {children}
         <div className="max-w-[1550px] mx-auto">
