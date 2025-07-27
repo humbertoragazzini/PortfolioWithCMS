29c808768907aad76a74ef263b17f95e6bae780e
humbertoragazzini <beto_vicio@hotmail.com>
2025-07-27 16:56:05 +0000

    building main

---
+++ src/components/atoms/Main.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+
+export default function Main({ children }: any) {
+  return (
+    <div>
+      <Canvas></Canvas>
+    </div>
+  );
+}
