3021d7e90522b9c550958b7e85ee3e7e2b32c7f4
humbertoragazzini <beto_vicio@hotmail.com>
2025-07-28 18:52:02 +0000

    moved the project to see them

--- src/slices/Project/index.tsx
+++ src/slices/Project/index.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 import { Html } from "@react-three/drei";
+import * as THREE from "three";
 
 /**
  * Props for `Project`.
@@ -22,9 +23,11 @@ const Project: FC<ProjectProps> = ({ slice }) => {
         slice.primary.order % 2 !== 0 ? Math.PI / 4 : -Math.PI / 4,
         0,
       ]}
-      position={[0, 0, -5 * (slice.primary.order ?? 0)]}
+      geometry={<planeGeometry args={[5, 5, 5]}></planeGeometry>}
+      material={<meshBasicMaterial></meshBasicMaterial>}
+      position={[-10 * (slice.primary.order % 2 !== 0 ? 1 : -1), 0, -5]}
     >
-      <div className="text-white bg-red-400">
+      <div className="text-white w-[calc(100vw-50px)]">
         <p>{slice.primary.title}</p>
         <p>{slice.primary.content}</p>
         {slice.primary.technologies.length > 0 && (
