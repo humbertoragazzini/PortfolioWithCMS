a17bf9f1ca65c2a1a5751565673d084e35094488
humberto ragazzini <beto_vicio@hotmail.com>
2025-08-03 16:38:04 +0000

    added menu state

--- src/components/html/organisms/OverlayMenu.tsx
+++ src/components/html/organisms/OverlayMenu.tsx
@@ -1,5 +1,6 @@
 "use client";
 import useStore from "@/hooks/zustand";
+import { motion } from "framer-motion";
 
 export default function OverlayMenu() {
   const menuOpen = useStore((state) => state.menuOpen);
@@ -37,11 +38,16 @@ export default function OverlayMenu() {
     <>
       <button
         className="fixed top-0 right-0 z-50 p-4 bg-red-400 hover:bg-amber-200"
-        onClick={}
+        onClick={toggleMenu}
       >
         Open menu
       </button>
-      <div className="fixed top-0 left-0 bg-[rgba(75,0,125,0.25)] backdrop-blur-md text-base z-40 h-screen w-screen">
+      <motion.div
+        animate={{
+          x: menuOpen ? "0vw" : "100vw",
+        }}
+        className="fixed top-0 left-0 bg-[rgba(75,0,125,0.25)] backdrop-blur-md text-base z-40 h-screen w-screen"
+      >
         <button
           className="p-4 bg-red-400 hover:bg-red-300 font-base"
           onClick={(e) => {
@@ -58,7 +64,7 @@ export default function OverlayMenu() {
         >
           Cambiar a espaniol
         </button>
-      </div>
+      </motion.div>
     </>
   );
 }
