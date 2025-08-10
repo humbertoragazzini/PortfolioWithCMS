2f2fcf3a0d9ef3c35dfaef04dae3cbedddc05ecb
humbertoragazzini <beto_vicio@hotmail.com>
2025-08-10 17:04:58 +0000

    TrackballControls removed

--- src/components/rtf/atoms/CanvasWrapper.tsx
+++ src/components/rtf/atoms/CanvasWrapper.tsx
@@ -7,7 +7,7 @@ import {
   MapControls,
   // OrbitControls,
   // PointerLockControls,
-  TrackballControls,
+  // TrackballControls,
 } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Model } from "../Models/Testing";
